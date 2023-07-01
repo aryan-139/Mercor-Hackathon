a4c8fb8c1b2dc0364947b8b92e07a0e84b9f31bc
Aryan Raj <aryanraj2k25@gmail.com>
2023-07-01 16:13:08 +0530

    login/register complete

---
+++ loginregister/src/Login.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { TextField, Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const StyledForm = styled('form')({
+  display: 'grid',
+  gap: '16.5px',
+});
+
+const StyledContainer = styled('div')({
+  display: 'flex',
+  justifyContent: 'space-between', // Align components to the right and left half of the screen respectively
+  alignItems: 'center',
+  gap: '16.5px',
+  padding: '16px',
+  backgroundColor: '#f5f5f5',
+  borderRadius: '18px',
+});
+
+const StyledLinkButton = styled(Button)({
+  textDecoration: 'underline',
+});
+
+const StyledAppName = styled('h1')({
+  fontFamily: 'Arial, sans-serif',
+  fontSize: '28px',
+  fontWeight: 'bold',
+  marginBottom: '16px',
+});
+
+const StyledSlideshow = styled('div')({
+  flex: '1', // Occupy remaining space
+  height: '100vh',
+  background: 'url(image1.jpg) center/cover', // Replace with the image source and customize as needed
+});
+
+export const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  }
+
+  return (
+    <StyledContainer className="auth-form-container">
+      <StyledSlideshow />
+      <div>
+        <StyledAppName>PrepPro</StyledAppName>
+        <StyledForm className="login-form" onSubmit={handleSubmit}>
+          <TextField
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            label="Email"
+            variant="outlined"
+          />
+          <TextField
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            type="password"
+            label="Password"
+            variant="outlined"
+          />
+          <Button type="submit" variant="contained">
+            Log In
+          </Button>
+        </StyledForm>
+        <StyledLinkButton
+          onClick={() => props.onFormSwitch('register')}
+          variant="text"
+        >
+          Don't have an account? Register here.
+        </StyledLinkButton>
+      </div>
+    </StyledContainer>
+  )
+}
---
+++ loginregister/src/Register.jsx
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import { TextField, Button, FormControlLabel, Checkbox, Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const StyledContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '16px',
+  padding: '16px',
+  backgroundColor: '#f5f5f5',
+  borderRadius: '12px',
+});
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+    const [college, setCollege] = useState('');
+    const [collegeId, setCollegeId] = useState('');
+    const [cgpa, setCgpa] = useState('');
+    const [passingYear, setPassingYear] = useState('');
+    const [phone, setPhone] = useState('');
+    const [location, setLocation] = useState('');
+    const [acceptTAndC, setAcceptTAndC] = useState(false);
+    const [confirmPass, setConfirmPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <StyledContainer className="auth-form-container">
+            <h2>Register</h2>
+            <form className="register-form" onSubmit={handleSubmit}>
+                <Box sx={{ width: '100%', maxWidth: '400px' }}>
+                    <TextField
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        type="text"
+                        label="Full Name"
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <TextField
+                        value={college}
+                        onChange={(e) => setCollege(e.target.value)}
+                        type="text"
+                        label="College"
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <TextField
+                        value={collegeId}
+                        onChange={(e) => setCollegeId(e.target.value)}
+                        type="text"
+                        label="College ID"
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <TextField
+                        value={cgpa}
+                        onChange={(e) => setCgpa(e.target.value)}
+                        type="text"
+                        label="CGPA"
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <TextField
+                        value={passingYear}
+                        onChange={(e) => setPassingYear(e.target.value)}
+                        type="text"
+                        label="Passing Year"
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <TextField
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        label="Email"
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <TextField
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        type="text"
+                        label="Phone No."
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <TextField
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                        type="text"
+                        label="Location"
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={acceptTAndC}
+                                onChange={(e) => setAcceptTAndC(e.target.checked)}
+                                id="acceptTAndC"
+                                name="acceptTAndC"
+                            />
+                        }
+                        label="Accept Terms & Conditions"
+                    />
+
+                    <TextField
+                        value={pass}
+                        onChange={(e) => setPass(e.target.value)}
+                        type="password"
+                        label="Password"
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <TextField
+                        value={confirmPass}
+                        onChange={(e) => setConfirmPass(e.target.value)}
+                        type="password"
+                        label="Confirm Password"
+                        variant="outlined"
+                        fullWidth
+                    />
+
+                    <Button type="submit" variant="contained">
+                        Register
+                    </Button>
+                </Box>
+            </form>
+            <Button
+                className="link-btn"
+                onClick={() => props.onFormSwitch('login')}
+                variant="text"
+            >
+                Already have an account? Login here.
+            </Button>
+        </StyledContainer>
+    );
+};
